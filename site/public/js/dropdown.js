fc1c1711f79b17890bc4767854d9462338fdf56c
itggot-jacob-sandstrom <jacob.sandstrom@elev.ga.ntig.se>
2019-01-27 16:01:42 +0100

    .

--- site/public/js/dropdown.js
+++ site/public/js/dropdown.js
@@ -1,7 +1,11 @@
 function dropdown(event){
     let dropdown = event.target.parentElement.querySelector(".dropdown")
     dropdown.classList.toggle("activate")
-    // document.write("works");
+
+    let x = event.target.parentElement.querySelector(".x")
+    x.classList.toggle("activate")
+    
+    console.log("toggle")
 }
 
 function registerContentBox(contentBox){
@@ -10,4 +14,22 @@ function registerContentBox(contentBox){
 
 let contentBoxes = document.querySelectorAll(".contentbox")
 contentBoxes.forEach(registerContentBox)
+
+function close(event){
+
+    let x = event.target.parentElement.parentElement.querySelector(".x")
+    x.classList.toggle("activate")
+    
+    let dropdown = event.target.parentElement.parentElement.querySelector(".dropdown")
+    dropdown.classList.toggle("activate")
+    
+    console.log("close")
+}
+
+function registerX(x){
+    x.addEventListener("click", close)
+}
+
+let xs = document.querySelectorAll(".x")
+xs.forEach(registerX)
 
